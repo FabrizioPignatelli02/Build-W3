2a5c2e33e0c73fc5759b45b99e5ee1dcec68daa0
Fabrizio Giuseppe Pignatelli <141920930+FabrizioPignatelli02@users.noreply.github.com>
2023-12-11 16:32:58 +0100

    detail page

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/post';
+import { PostService } from 'src/app/services/post.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.scss']
+  styleUrls: ['./detail.component.scss'],
 })
 export class DetailComponent implements OnInit {
-
-  constructor() { }
+  idpost: number = 0;
+  constructor(private route: ActivatedRoute, private srvPost: PostService) {}
 
   ngOnInit(): void {
+    if (this.route.snapshot.paramMap.get('id')) {
+      this.idpost = +this.route.snapshot.paramMap.get('id')!;
+      console.log('id', this.idpost);
+    }
   }
-
 }
