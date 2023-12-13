13699ac8cd5c6cfbc35898d5c900d56c2a89490e
Luca-Favaretto <145663602+Luca-Favaretto@users.noreply.github.com>
2023-12-13 12:42:12 +0100

    file password recovery

--- src/app/auth/password-recovery/password-recovery.component.ts
+++ src/app/auth/password-recovery/password-recovery.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { PasswordRecoveryService } from '../password-recovery.service';
 
 @Component({
   selector: 'app-password-recovery',
   templateUrl: './password-recovery.component.html',
-  styleUrls: ['./password-recovery.component.scss']
+  styleUrls: ['./password-recovery.component.scss'],
 })
 export class PasswordRecoveryComponent implements OnInit {
+  constructor(private srvPassRec: PasswordRecoveryService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  async sendEmail() {
+    try {
+      await this.srvPassRec.sendEmail(
+        'giuseppe.triolo99@gmail.com',
+        'password',
+        'hello world',
+        '<p>contenuto mail</p>'
+      );
+      console.log('Email inviata con successo');
+    } catch (error) {
+      console.error("Errore durante l'invio dell'email:", error);
+    }
   }
-
+  ngOnInit(): void {}
 }
