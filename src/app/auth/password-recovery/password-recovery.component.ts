cef6e70b534c2beca17460a75a0bf9f8a5ee56bc
Luca-Favaretto <145663602+Luca-Favaretto@users.noreply.github.com>
2023-12-13 14:37:18 +0100

    fix luca, add posts poster

--- src/app/auth/password-recovery/password-recovery.component.ts
+++ src/app/auth/password-recovery/password-recovery.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { PasswordRecoveryService } from '../password-recovery.service';
+// import { PasswordRecoveryService } from '../password-recovery.service';
 
 @Component({
   selector: 'app-password-recovery',
@@ -7,20 +7,20 @@ import { PasswordRecoveryService } from '../password-recovery.service';
   styleUrls: ['./password-recovery.component.scss'],
 })
 export class PasswordRecoveryComponent implements OnInit {
-  constructor(private srvPassRec: PasswordRecoveryService) {}
+  // constructor(private srvPassRec: PasswordRecoveryService) {}
 
-  async sendEmail() {
-    try {
-      await this.srvPassRec.sendEmail(
-        'giuseppe.triolo99@gmail.com',
-        'password',
-        'hello world',
-        '<p>contenuto mail</p>'
-      );
-      console.log('Email inviata con successo');
-    } catch (error) {
-      console.error("Errore durante l'invio dell'email:", error);
-    }
-  }
+  // async sendEmail() {
+  //   try {
+  //     await this.srvPassRec.sendEmail(
+  //       'giuseppe.triolo99@gmail.com',
+  //       'password',
+  //       'hello world',
+  //       '<p>contenuto mail</p>'
+  //     );
+  //     console.log('Email inviata con successo');
+  //   } catch (error) {
+  //     console.error("Errore durante l'invio dell'email:", error);
+  //   }
+  // }
   ngOnInit(): void {}
 }
